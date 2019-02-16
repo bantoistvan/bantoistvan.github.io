8f934f16f328c5ee75de2f6bbc23c99e5e8db633
Istvan banto <bantoistvan@gmail.com>
2019-02-16 15:43:54 +0000

    web-ready

---
+++ precache-manifest.b49070164a4e8dd214697061457f9bf6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "98883932c1b6482b5e71",
+    "url": "/static/css/main.ba0213f8.chunk.css"
+  },
+  {
+    "revision": "98883932c1b6482b5e71",
+    "url": "/static/js/main.98883932.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "412d8f46f21574a001eb6b8b2f16638d",
+    "url": "/static/media/logo.412d8f46.svg"
+  },
+  {
+    "revision": "d8cfd737778ae17f1d6fac5bfc1ad3a9",
+    "url": "/index.html"
+  }
+];
